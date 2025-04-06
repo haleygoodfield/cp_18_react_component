eb7dcf136a99f6347bc9fa3e4227541bdafde8b3
Haley Goodfield <haleygoodfield@usf.edu>
2025-04-06 12:56:01 -0400

    Added "Import React" in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 // Task 2: Use the Component
+import React from "react"; // Import React
 import Greeting from './components/Greeting'; // Import the Greeting component
 
 function App() {
